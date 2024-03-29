fa61dd50f029b912a27a0f0ff73ade92d2271c8a
Arthur Kamsu <arthur.kamsu@lemak-soft.com>
2024-03-29 17:52:10 -0500

    Add the Fsharp project - Adds the EmployeeIdWithTargetSalary type - Adds the Raiseanager module with the AddRaise function

---
+++ sources/FsharpDotnet/EmployeeIdWithTargetSalary.fs
@@ -0,0 +1,9 @@
+﻿namespace FsharpDotnet
+
+type EmployeeIdWithTargetSalary(employeeId: int, currentSalary: decimal, targetSalary: decimal) =
+    member this.EmployeeId : int = employeeId
+    member this.CurrentSalary: decimal = currentSalary
+    member this.TargetSalary: decimal = targetSalary
+
+    member this.ToString() = 
+        printf "{EmployeeId: %d, CurrentSalary: %M, TargetSalary: %M}", this.EmployeeId, this.CurrentSalary, this.TargetSalary
---
+++ sources/FsharpDotnet/RaiseManager.fs
@@ -0,0 +1,12 @@
+﻿namespace FsharpDotnet
+
+module RaiseManager =
+    let AddRaise(employeeIdsWithInitialSalary: (struct (int * decimal)) list, percentage: int) = 
+        if percentage > 100 || percentage < 1 then
+            raise (invalidOp "Invalid percentage (Must be between 1 and 100).")
+
+        employeeIdsWithInitialSalary
+        |> List.map(fun struct (employeeId, initialSalary) -> EmployeeIdWithTargetSalary(
+        employeeId,
+        initialSalary,
+        initialSalary + (initialSalary * decimal percentage / decimal 100)))
