e19e8dfcecfe679ef0de4bc49139e9b32f1ed3ff
Arthur Kamsu <arthur.kamsu@lemak-soft.com>
2024-03-29 17:53:34 -0500

    Adds the CSharp project - Adds the TableRecords type - Adds the Display Helper with the DisplayTable function

---
+++ sources/CsharpDotnet/DisplayHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace CsharpDotnet;
+
+public static class DisplayHelper
+{
+    public static string DisplayTable(IEnumerable<TableRecords> tableRecords)
+    {
+        StringBuilder sb = new();
+        sb.AppendLine("FullName\t\t\tID\tCurrent Salary\tTarget Salary\n");
+
+        decimal totalCurrent = 0;
+        decimal totalTarget = 0;
+
+        foreach (var item in tableRecords)
+        {
+            totalCurrent += item.CurrentSalary;
+            totalTarget += item.TargetSalary;
+            sb.AppendLine($"{item.FullName}\t\t\t{item.Id}\t{item.CurrentSalary:C}\t{item.TargetSalary:C}");
+        }
+        sb.AppendLine($"\n\t\t\t\tTotal of Current Salaries:\t{totalCurrent:C}");
+        sb.Append($"\t\t\t\tTotal of Target Salaries:\t{totalTarget:C}");
+
+        return sb.ToString();
+    }
+}
---
+++ sources/CsharpDotnet/TableRecords.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpDotnet;
+
+public class TableRecords
+{
+    public int Id { get; set; }
+    public decimal CurrentSalary { get; set; }
+    public decimal TargetSalary { get; set; }
+    public string FullName { get; set; } = string.Empty;
+}
